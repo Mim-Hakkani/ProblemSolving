c08955921f750f6c280debf20d5fd9c45e3a245b
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-07 17:33:37 +0600

    check the if else condition

---
+++ Condition/age.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(){
+int age = 18 ;
+if(age<18){
+    cout<<"you are not a voter "<<endl;
+}
+else{
+    cout<<"Yes !! I am voter .. now"<<endl;
+}
+return 0;
+}
