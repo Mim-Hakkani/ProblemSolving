cc5671dc1b55f5a85d7844e5d0ebe84db51d43f8
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-08 12:04:31 +0600

    using structure show the data

---
+++ Structure/personalinfo.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+struct personalinfo
+{
+    /* data types */
+
+    string name,month;
+    int date,year;
+    long int phone;
+
+};
+
+int main(){
+ // declare it as a variables 
+
+// hakknai data 
+ struct personalinfo pInfo;
+ pInfo.name ="Golam Hakkani Mim";
+ pInfo.month ="August";
+ pInfo.date =03;
+ pInfo.year =1994;
+ pInfo.phone =745166705;
+
+
+ // Ema data 
+ struct personalinfo pInfo2;
+ pInfo2.name ="Suhana AfaZ Emu";
+ pInfo2.month ="August";
+ pInfo2.date =02;
+ pInfo2.year =1995;
+ pInfo2.phone =745166905;
+
+ // print the personal informations 
+ cout<<"Name :"<<pInfo.name<<endl;
+ cout<<"DOB :"<< pInfo.month<<" "<<pInfo.date<<" "<<pInfo.year<<endl;
+ cout<<"Phone :"<<pInfo.phone<<endl;
+
+cout<<endl;
+ // print the personal informations 2 
+ cout<<"Name :"<<pInfo2.name<<endl;
+ cout<<"DOB :"<< pInfo2.month<<" "<<pInfo2.date<<" "<<pInfo2.year<<endl;
+ cout<<"Phone :"<<pInfo2.phone<<endl;
+ 
+}
--- Structure/structureexp.cpp
+++ Structure/structureexp.cpp
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<iostream>
 // Create a structure called myStructure
 struct myStructure {
   int myNum;
