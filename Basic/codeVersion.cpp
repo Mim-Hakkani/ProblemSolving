f7870f2f23f968dbca08d564a7745d80dfd817e8
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-08 18:31:57 +0600

    Show my name in print function

---
+++ Basic/codeVersion.cpp
@@ -0,0 +1,18 @@
+
+/*
+
+problem Name : Write a C program to get the C version you are using. Go to the editor
+
+input : 
+
+output :
+      We are using C18!
+
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+   cout<<"You Are Using C16!"<<endl;
+   return 0; 
+}
