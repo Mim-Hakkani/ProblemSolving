978fcb7c679c24d6e58271b8a783b26bde5be32d
Hakkani Mim <mimhakkani4g@gmail.com>
2022-09-16 12:15:54 +0600

    reminder by 3 app

--- Condition/Reminder3Number.cpp
+++ Condition/Reminder3Number.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int num;
+    int num,count=0;
     cout<<"Enter The Number : ";
     cin>>num;
 
     // condition 
       
       for(int i=1;i<=100;i++){
-        if(num%i==3){
+        if(i%num==3){
+            count++;
             cout<<i<<endl;
         }
       }
