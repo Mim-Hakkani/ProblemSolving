7cdfbd682ef6af3379378edac21b0ebe168a80d8
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-18 10:58:28 +0600

    Quadrant number

--- Basic/passwordCheck.cpp
+++ Basic/passwordCheck.cpp
@@ -1,3 +1,11 @@
+/*
+36. Write a C program to read a password until it is correct. For wrong password print "Incorrect password" and for correct password print "Correct password" and quit the program. The correct password is 1234. Go to the editor
+Test Data :
+Input the password: 1234
+Expected Output:
+Correct password
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
