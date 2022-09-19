c077013b8432b80a3121ca885c7b3b959c7830d3
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-19 12:51:41 +0600

    series two is done

--- Loop/Series/series2.cpp
+++ Loop/Series/series2.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std ;
+/*
+
+45. Write a C program to calculate the value of S where S = 1 + 1/2 + 1/3 + … + 1/50. Go to the editor
+
+Expected Output:
+Value of S: 4.50
+
+*/
+
 int main(){
     int n;
     float sum =1;
