e18ba3d559bc683d64c0db3860627723077e7107
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-20 16:43:09 +0600

    series pattern of plus and minus section

--- Loop/Series/Series12.cpp
+++ Loop/Series/Series12.cpp
@@ -1,5 +1,5 @@
 /*
-Series pattern :1-2+3-4+5-6
+Series pattern :1-2+3-4+5-6 = -3 
 */
 
 #include<iostream>
@@ -12,9 +12,9 @@ int main(){
 
     for (i=1,j=2;i<=number,j<=number*2;i+=2,j+=2)
     {
-      cout<<i<<"."<<j<<endl;
+    //   cout<<i<<"."<<j<<endl;
        sum =sum+(i-j);
-       cout<<sum<<endl;
+    //    cout<<sum<<endl;
        
     }
     
