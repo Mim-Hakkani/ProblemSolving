b9fe22e559b7cddf7a2e52049b54957a913d73e0
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-20 16:08:35 +0600

    add the squre series in project

--- Loop/Series/series7.cpp
+++ Loop/Series/series7.cpp
@@ -1,21 +1,29 @@
 /*
-Series pattern :1.5+2.5+3.5+...+n.5
+Series pattern :1^2 + 2^2 + 3^3 +....n^2
 */
 
 #include<iostream>
 using namespace std ;
 
 int main(){
-    int number,sum=0;
+    int number,sum=0,another;
     cout<<"Enter Num : ";
     cin>>number;
 
     for (int i = 1; i <=number ; i++)
     {
-      cout<<i<<"."<<5<<endl;
-       sum =sum+i*5;
+      cout<<i<<"^"<<i<<endl;
+       sum =sum+i*i;
+
+       
     }
     
-    cout<<"Summation of another Series is :: "<<sum<<endl;
+    // another pattern 
+
+    another = (number*(number+1)*(2*number+1))/6;
+
+
+    cout<<"Summation of Loop Series is :: "<<sum<<endl;
+    cout<<"Summation of another Series is :: "<<another<<endl;
     return 0;
 }
