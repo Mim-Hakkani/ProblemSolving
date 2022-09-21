8d9947285a114e3ee0ab14d9e78f04c440f694d9
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-21 17:36:37 +0600

    show pattern in another way

--- Loop/Pattren/star2.cpp
+++ Loop/Pattren/star2.cpp
@@ -1,10 +1,9 @@
 /*
 
 Pattern Series :   
-*
-**
-***
-****
+*****
+*****
+*****
 
 */
 
@@ -18,7 +17,7 @@ cout<<"How Many Star Do You Print : ";
 cin>>n;
 
 for(i=1;i<=n;i++){
-    for(j=1;j<=i;j++){
+    for(j=1;j<=n;j++){
         cout<<"* ";
     }
     cout<<"\n";
