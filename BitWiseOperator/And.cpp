c5480d6246b0ae14e301f8de75eb912bb8ea480e
32-Mimhakkani <mim.cse32@gmail.com>
2022-09-24 18:11:22 +0600

    BitWise And operator

---
+++ BitWiseOperator/And.cpp
@@ -0,0 +1,17 @@
+/*
+a = 3
+b = 4 
+
+c = 0 
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int a = 3, b= 4,c;
+    c  = a & b;
+    cout << "C BitWise And Operation "<<c<<"\n";
+
+    return 0;
+}
