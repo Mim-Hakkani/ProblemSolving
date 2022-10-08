c107b4f6d3906ae10ae20eacb05fd50801211904
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-08 18:29:44 +0600

    exam first even odd numebr is done

---
+++ Loop/Basic/exam.cpp
@@ -0,0 +1,93 @@
+/*
+
+Q6 [15 marks]
+
+
+Take two natural numbers X and Y. If X * Y = Z, is Z even or odd when X and Y -
+
+
+(Case 1) both are even: even / odd
+
+(Case 2) both are odd: even / odd
+
+(Case 3) One of them is odd, the other one is even: even / odd
+
+
+
+
+For Case 1 : 
+
+       if both are even 
+
+       let x = 10
+           y = 4
+
+           x*y = z
+
+           z = x*y
+           z = 10*4 
+           z = 40 
+
+           we know that a number or z is even when z%2 = 0
+
+           in this equation , see that  z = 40 is 40%2 = 0 so 
+           z is even number always 
+
+           so finally we say that ,
+
+            even * even = even 
+
+
+
+Case 2 : 
+
+ if both are odd 
+
+       let x = 3
+           y = 5
+
+           x*y = z
+
+           z = x*y
+           z = 3*5 
+           z = 15
+
+           we know that a number or z is odd when z%2 = 1
+
+           in this equation , see that  z = 15 is 15%2 = 1 so 
+           z is odd number always 
+
+          so we say that odd * odd = odd 
+
+
+Case 3: 
+  One of them is odd, the other one is even: even / odd
+
+
+       let x = 4
+           y = 5
+
+           x*y = z
+
+           z = x*y
+           z = 4*5 
+           z = 40
+
+           we know that a number or z is odd when z%2 = 1 or a number or z is even when z%2 = 0
+
+           in this case we see that 
+            
+            even * odd  = even  => 5 * 4 = 40 that is even 
+            odd * even  = even  => 4 * 5 = 40 that is even
+
+           in this equation , see that  z = 40 is 40%2 = 0 so 
+           z is even number always 
+
+final Case 
+  
+   one is odd and another is odd  that the number is = odd 
+
+   5 * 5 = 25 is odd 
+   3 * 7  = 21 is odd 
+                  
+*/
