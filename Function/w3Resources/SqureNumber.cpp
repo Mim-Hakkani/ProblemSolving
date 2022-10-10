3c1f0967538f270d7d30b1b9e5138211c2bea266
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-10 18:16:40 +0600

    swap the integer number

--- Function/w3Resources/SqureNumber.cpp
+++ Function/w3Resources/SqureNumber.cpp
@@ -1,9 +1,45 @@
 /*
 
-problem Name : Write a program in C to find the square of any number using the function.
+ Write a program in C to swap two numbers using function. Go to the editor
+Test Data :
+Input 1st number : 2
+Input 2nd number : 4
+Expected Output :
 
-inputOutput : 
-The total is :  11  
+Before swapping: n1 = 2, n2 = 4                                                  
+After swapping: n1 = 4, n2 = 2  
 
 
 */
+
+
+#include<iostream>
+using namespace std ;
+
+int SwapNumber(int a,int b){
+    
+    int temp ;
+
+     temp = a; 
+     a=b;
+     b=temp;
+     cout<<"Swap Number :"<<a<<" "<<b<<"\n";
+    
+}
+
+int main(){
+    int a,b;
+    cout<<"Enter Number : ";
+    cin>>a;
+
+    cout<<"Enter Number2: ";
+    cin>>b;
+
+    cout<<"Two Number is : " << a << b<<"\n"; 
+
+       SwapNumber(a,b);
+
+    return 0;
+}
+
+
