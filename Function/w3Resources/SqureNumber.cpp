8403d0edb02ac91bf3e9211d8e90f5c8c77dc789
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-10 17:18:54 +0600

    squre root

---
+++ Function/w3Resources/SqureNumber.cpp
@@ -0,0 +1,9 @@
+/*
+
+problem Name : Write a program in C to find the square of any number using the function.
+
+inputOutput : 
+The total is :  11  
+
+
+*/
--- Function/w3Resources/TotalDonation.cpp
+++ Function/w3Resources/TotalDonation.cpp
@@ -2,31 +2,30 @@
 problem Name : Write a program in C to show the simple structure of a function
 
 inputOutput : 
-The total is :  11  
+
+Input any number for square : 20
+
 */
 
 #include<iostream>
 using namespace std ;
 
-//  funtion declaration 
-
-int TotalDonate (int a,int b){
-    cout<<"Enter Donation 1: ";
-    cin>>a;
-
-    cout<<"Enter Donation 2: ";
-    cin>>b;
-
-    int total = a+b;
-
-    return total;
-}
+int SqureNumber(int a );
 
 int main(){
-    int donate1,donate2;
+    int a;
     
-    int TotalValue = TotalDonate(donate1,donate2);
-    cout<<"Total Donation is : "<<TotalValue<<"\n";
+    int TotalValue = SqureNumber(a);
+    cout<<"Squre Number is : "<<TotalValue<<"\n";
 
     return 0;
+}
+
+
+int SqureNumber(int a){
+    cout<<"Enter The Number : ";
+    cin>>a;
+    int squre = a*a;
+
+    return squre ;
 }
