effd21bcef8fab51ff54dc20c5c9fbe49d0afb72
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-10-18 18:12:28 +0600

    phitron simple problem is done

---
+++ Basic/phitronPraticeProblem-1/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+/*
+problem Name : print basic advanced problems
+
+*/
+
+#include<stdio.h>
+int main(){
+    printf("Hi there /\\ \t I hope you are doing graet and praticing well.\nNow this is a simple task yopu need to do.\t Just care for the indentation.\n Show 100%% of sincerity. \n");
+}
