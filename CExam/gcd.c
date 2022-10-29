8dbee651c23784d391a8355a4e3c408991ad4361
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-10-29 15:44:57 +0600

    solved the gcd problem

--- CExam/gcd.c
+++ CExam/gcd.c
@@ -3,7 +3,7 @@
 int main(){
     int num1,num2,temp,rem;
     scanf("%d %d",&num1,&num2);
-    printf("The GCD of %d and %d is ",num1,num2);
+    printf("The GCD of %d and %d is :",num1,num2);
 
     while(num2!=0){
       rem = num1%num2;
