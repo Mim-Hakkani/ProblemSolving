86fb28422791abfc51a143bf9b33217530ff8659
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-10-29 17:14:16 +0600

    prime number is solved

--- CExam/tempCodeRunnerFile.c
+++ CExam/tempCodeRunnerFile.c
@@ -1,26 +1,22 @@
 #include<stdio.h>
 int main(){
-    int number,Number,i;
+    int number,Number,i,flag=1;
     scanf("%d",&number);
     if(number==2){
-        printf("Prime\n");
+       flag=1;
     }
     else if(number%2==0 || number<0 || number==1)
     {
-        printf("Composite\n");
+        flag=0;
         
     }
     
-    else{
-          
-     i = 3;
-            while(number>=i){
-                if(number%i==0){ printf("Composite\n");break;}
-                else{
-              printf("Prime\n");
-                     break;
-                }
-                i++;
-            }
+    else{        
+          for(i=3;i<=number/2;i++){
+            if(number%i==0){flag=0;break;}
+          }
     }
+
+    if(flag==0) printf("Composite\n");
+    if(flag==1) printf("Prime\n");
 }
