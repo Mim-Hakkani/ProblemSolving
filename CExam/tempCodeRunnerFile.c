68c849b335d0d4b762df04ab139e98c350693eeb
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-10-29 17:55:10 +0600

    solved

--- CExam/tempCodeRunnerFile.c
+++ CExam/tempCodeRunnerFile.c
@@ -1,22 +1,33 @@
-#include<stdio.h>
-int main(){
-    int number,Number,i,flag=1;
-    scanf("%d",&number);
-    if(number==2){
-       flag=1;
-    }
-    else if(number%2==0 || number<0 || number==1)
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d ,",n);
+
+    while (n != 1)
     {
-        flag=0;
-        
-    }
-    
-    else{        
-          for(i=3;i<=number/2;i++){
-            if(number%i==0){flag=0;break;}
-          }
+
+        if (n % 2 == 0)
+        {
+            n /= 2;
+            if (n == 1)
+            {
+                printf("%d\n", n);
+            }
+            else
+            {
+                printf("%d,", n);
+            }
+        }
+
+        else
+        {
+
+            n -= 1;
+            printf("%d,", n);
+        }
     }
 
-    if(flag==0) printf("Composite\n");
-    if(flag==1) printf("Prime\n");
+    return 0;
 }
