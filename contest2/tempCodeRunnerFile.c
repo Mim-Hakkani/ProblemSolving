84decf48435a9036c7cbdc18eb6abf07727fb58f
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-03 23:33:42 +0600

    solved the missing number

---
+++ contest2/tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+   int t,sumTotal,a,b,c,missing,i;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++){
+
+         scanf("%d %d %d %d",&sumTotal,&a,&b,&c);
+         if(sumTotal >0 && a>0 && b>0 && c>0){
+
+          missing = sumTotal - (a+b+c);
+          printf("%d\n",missing);
+         }
+
+    }
+  
+    return 0;
+}
