c093ce1f0b40651343aa588655fb18e4282bf3a7
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-07 15:31:07 +0600

    space count using string

--- String/spaceCount.cpp
+++ String/spaceCount.cpp
@@ -4,7 +4,7 @@ vowel number count
 #include<iostream>
 using namespace std;
 int main(){
-    char companyName[] ="Ehsan Marketing is the Best Company That i have seen. ";
+    char companyName[] ="Ehsan Marketing is the Best Company That i have seen.";
     int i =0,count=0;
 
     while (companyName[i]!='\0')
--- String/vowel.cpp
+++ String/vowel.cpp
@@ -1,3 +1,6 @@
+/*
+vowel number count 
+*/
 #include<iostream>
 using namespace std;
 int main(){
