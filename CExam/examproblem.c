d2d0ddc02691592476dfbc6c2846c450613164a3
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-16 19:02:05 +0600

    exam question is upload

--- CExam/examproblem.c
+++ CExam/examproblem.c
@@ -96,3 +96,33 @@ for(i=1,k=0;k<=15;i*=2){                                                        
 
 // *********************************************************
 There are 48 byte in the int type array with 12 elements in memory.
+
+/*****************************************************************/
+
+What does lexicographical comparison mean? Explain with examples. How can we order strings in lexicographic order in C? 	   (10)				  	      
+ 
+What does ASCII value of a character mean? How can you print the ASCII value of a character in C language?      (10)
+
+Explain Bubble Sorting. Suppose you are given an array of integers 12, 7, 9, 1, 3, 73, 11, 15, 62, 19, 4. What will be the sequence of these integers if we run Bubble sort for only 5 iterations?      (10)
+
+How can you find the sum of digits of a number? Write a C program that will extract the digits of the given integer number as input and add them to find the required output. For example, if the input is 1235623, then the output will be 22. Because, 1+2+3+5+6+2+3 = 22.        (6)
+
+You will be given a 3x3 matrix as input. You need to check whether the matrix is a magic square or not. Magic squares are the matrices which have equal sum along all rows, columns and diagonals. For example,
+
+This is a magic square as you can see all the sums are equal to 15 along all the rows and columns and even the diagonals.
+Now,Write a C Program to check the matrix is magic square or not.     (10)
+
+
+  Sample Input	
+  Sample Output
+  2   7   6
+  9   8   1                                                                                    
+  4   3   5
+
+ NO
+  2   7   6
+  9   5   1                                                                                    
+  4   3   8
+
+   YES
+
