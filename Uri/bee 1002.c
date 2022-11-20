25d023b1fe42db5aef9e947e203b4f4e6e2c33c6
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-20 19:43:56 +0600

    bee 1003 problem is solved

--- Uri/bee 1002.c
+++ Uri/bee 1002.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 int main(){
     double r,result,pi = 3.14159;;  
-    scanf("%0.4lf",&r);
-    printf("%lf\n",r);
-
+    scanf("%lf",&r);
     result = pi*r*r;
     printf("A=%0.4lf\n",result);
     return 0;
-
-
     
     }
