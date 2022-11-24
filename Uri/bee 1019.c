c15a2db85e8838c7fbdce93c36ad79707a82c9c8
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-24 11:42:44 +0600

    1019 problem is solved

--- Uri/bee 1019.c
+++ Uri/bee 1019.c
@@ -5,13 +5,20 @@
 int main(){
     int time,remaining,result;
     scanf("%d",&time);
+    
+    // calculate for hour 
+
     result = time/3600;
     remaining = time -(result*3600);
     printf("%d:",result);
 
+    // calculate for minites 
+
     result = remaining/60;
     remaining = remaining -(result*60);
     printf("%d:",result);
+
+    // calculate for seconds 
     printf("%d\n",remaining);
 
 
