92b56951163dd3e18031c885f902e5b6aa533eaa
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-28 15:55:46 +0600

    1074 is solved

---
+++ Uri/bee 1074.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+   int N,X,i;
+   scanf("%d",&N);
+   for(i=1;i<=N;i++)
+   {
+      scanf("%d",&X);
+
+   if(X>0 && X%2==0)
+   {
+      printf("EVEN POSITIVE\n");
+   }
+   if(X<0 && X%2==0)
+   {
+      printf("EVEN NEGATIVE\n");
+   }
+   if(X>0 && X%2!=0)
+   {
+      printf("ODD POSITIVE\n");
+   }
+    if(X<0 && X%2!=0)
+   {
+      printf("ODD NEGATIVE\n");
+   }
+   if(X==0)
+   {
+      printf("NULL\n");
+   }
+   }
+   return 0;
+}
