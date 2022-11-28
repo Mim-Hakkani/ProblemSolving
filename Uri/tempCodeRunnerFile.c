5890db5a7e21a7b4be52bbac9c2dcce69cbf6498
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-28 14:45:07 +0600

    bee 1021 is solved

--- Uri/tempCodeRunnerFile.c
+++ Uri/tempCodeRunnerFile.c
@@ -2,27 +2,38 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-int main(){
-    int days,remaining,result;
-    scanf("%d",&days);
-    
-    // calculate for hour 
+int main()
+{
+    float taka, remaining, result,somechanges;
+    int remaining2;
+    scanf("%f", &taka);
+    remaining2 = taka*100.00;
+     printf("%d\n",remaining2);
 
-    result = days/365;
-    remaining = days -(result*365);
-    printf("%d ano(s)\n",result);
+    // remaining = taka - (result * 100);
+    // printf("%d\n",taka);
+    // printf("%d nota(s) de R$ 100,00\n", result);
 
-    // calculate for minites 
+    // result = remaining / 50;
+    // remaining = remaining - (result * 50);
+    // printf("%d nota(s) de R$ 50,00\n", result);
 
-    result = remaining/30;
-    remaining = remaining -(result*30);
-    printf("%d mes(es)\n",result);
+    // result = remaining / 20;
+    // remaining = remaining - (result * 20);
+    // printf("%d nota(s) de R$ 20,00\n", result);
 
-    // calculate for seconds 
-    printf("%d dia(s)\n",remaining);
+    // result = remaining / 10;
+    // remaining = remaining - (result * 10);
+    // printf("%d nota(s) de R$ 10,00\n", result);
 
+    // result = remaining / 5;
+    // remaining = remaining - (result * 5);
+    // printf("%d nota(s) de R$ 5,00\n", result);
 
-
+    // result = remaining / 2;
+    // remaining = remaining - (result * 2);
+    // printf("%d nota(s) de R$ 2,00\n", result);
+    // printf("%d nota(s) de R$ 1,00\n", remaining);
 
     return 0;
 }
