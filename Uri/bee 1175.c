f9c7fd8e43d614219b832edfd8728ae209218ea5
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-11-28 18:24:39 +0600

    1175 is solved

---
+++ Uri/bee 1175.c
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[20];
+    for(int i=0;i<20;i++){
+        cin>>a[i];
+
+    }
+        for(int j=19,i=0;j>=0,i<20;j--,i++)
+        {
+            cout<<"N["<<i<<"] = "<<a[j]<<endl;
+        }
+
+        return 0;
+
+}
