4baadb6df08dcc11d4b7ff2a739e6e5645a5576b
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-01 17:57:39 +0600

    problem 2 bug is fixed

--- CExam2/tempCodeRunnerFile.c
+++ CExam2/tempCodeRunnerFile.c
@@ -1,40 +1,19 @@
-/*
-Write a C program that will take a string  as input from you and change the cases of the characters.If the letter is capital, change it into small and if the letter is small change it into capital letter. Look at the test case and explanation for more clarification.
-
-Sample Input:
-hElLoWorld
-
-Sample Output:
-HeLlOwORLD
-
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-int main()
+int main(){
+
+int i=0;
+while(i<10)
 {
-    char changes[1000];
-    fgets(changes, sizeof(changes), stdin);
+	printf("I am inside the loop \:%d \n",i);
 
-    int i = 0;
-    while (changes[i] != '\0')
-    {
+    // correction 
 
-        if (changes[i] >= 'a' && changes[i] <= 'z')
-        {
-            changes[i] = changes[i] - 32;
-        }
-        else if (changes[i] >= 'A' && changes[i] <= 'Z')
-        {
-            changes[i] = changes[i] + 32;
-        }
-
-        i++;
-    }
-
-    puts(changes);
+    i++;
+}
+  
 
     return 0;
 }
