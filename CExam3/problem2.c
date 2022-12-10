d479b84ce10b2959dad9e9ca826487453ec685d2
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-10 10:58:36 +0600

    solved the second problem

--- CExam3/problem2.c
+++ CExam3/problem2.c
@@ -9,3 +9,52 @@ ssddccba
 
 
 */
+
+#include<stdio.h>
+#include<string.h>
+
+void reverseString(char name[]){
+//   printf("Output is : ");
+// puts(name);
+
+int stringLength = strlen(name);
+
+// printf("Before Sort : ");
+
+//  for (int i = 0; i <stringLength; i++)
+//  {
+//     printf("%c",name[i]);
+//  }
+
+
+char temp;
+
+for(int i=0;i<stringLength;i++){
+    for(int j=0;j<stringLength-1;j++){
+        if(name[j]<name[j+1]){
+            temp=name[j];
+            name[j]=name[j+1];
+            name[j+1]=temp;
+
+        }
+    }
+}
+ 
+ // show the output 
+
+//  printf("\nafter sort : ");
+
+ for (int i = 0; i <stringLength; i++)
+ {
+    printf("%c",name[i]);
+ }
+ 
+
+}
+
+int main(){
+    char name[100];
+    fgets(name,sizeof(name),stdin);
+    reverseString(name);
+return 0;
+}
--- CExam3/tempCodeRunnerFile.c
+++ CExam3/tempCodeRunnerFile.c
@@ -1,53 +1,60 @@
 /*
-You have given a series, 1+2+3-4-5-6+7+8+9-10-11-12+......N . Your task to print the sum of the Nth element.
+You have given a string. Now your task is to sort this string in descending order. Implement this using function.
 
 Sample Input 1
-10
+abddccss
 
 Sample Output 1
-5
+ssddccba
 
-Sample Input 2
-20
-
-Sample Output 2
-12
-
-Explanation of Sample Input 2:
-1+2+3-4-5-6+7+8+9-10-11-12+13+14+15-16-17-18+19+20 = 12. Sum up to the 20th of this series.
 
 */
 
-
 #include<stdio.h>
-int main(){
-    int i,j,n,sum=0,count=0,c2=0;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        count++;
-        // printf("count : %d\n",count);
-        if(count>3){
-            //  printf("count greater 3 : %d\n",count);
-            //  printf("count greater i : %d\n",i);
-            sum = sum-i;
-            // printf("Sum: %d\n",sum);
+#include<string.h>
+
+void reverseString(char name[]){
+//   printf("Output is : ");
+// puts(name);
+
+int stringLength = strlen(name);
+
+// printf("Before Sort : ");
+
+//  for (int i = 0; i <stringLength; i++)
+//  {
+//     printf("%c",name[i]);
+//  }
 
 
-            if(i%3==0) { 
-                count =0;
-                }
+char temp;
 
+for(int i=0;i<stringLength;i++){
+    for(int j=0;j<stringLength-1;j++){
+        if(name[j]<name[j+1]){
+            temp=name[j];
+            name[j]=name[j+1];
+            name[j+1]=temp;
 
         }
-        else{
-          sum = sum+i;
-        }
-        
-
-   
     }
+}
+ 
+ // show the output 
 
-    printf("%d\n",sum);
-    
-    return 0;
+//  printf("\nafter sort : ");
+
+ for (int i = 0; i <stringLength; i++)
+ {
+    printf("%c",name[i]);
+ }
+ 
+
+}
+
+int main(){
+    char name[100];
+    fgets(name,sizeof(name),stdin);
+    reverseString(name);
+return 0;
 }
