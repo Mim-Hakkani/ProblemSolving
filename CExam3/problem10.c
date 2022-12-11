10d2ba650f95a589d2b57ba6824bfeb960a17fd6
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-11 19:22:15 +0600

    beautiful array problem

---
+++ CExam3/problem10.c
@@ -0,0 +1,36 @@
+/*
+Question-10
+ Beautiful Array
+
+Problem Statement:
+You are given an array of size n. And the next line will contain n positive integers. Your favourite number is 7. The array will be nice if half or more of the numbers in the array have 7 digits. Implement it using a function and traverse the array using pointers.
+
+See the sample input-output for more clarification.
+
+Note - If the array size is odd that time  consider the ceil value as half, for example n=5, that means n/2 = 5/2 = 3 
+
+Sample Input- 1
+-----------------------
+6
+33 1 17 171 88 734
+
+Sample Output- 1
+-----------------------
+Beautiful
+
+Sample Input- 2
+-----------------------
+5
+33 1 17 11 88 
+
+Sample Output- 2
+-----------------------
+Ugly
+
+Explanation - 
+In sample input 1 - 
+17 have the last digit 7
+171 have the middle digit 7
+734 has the first digit 7 
+
+*/
