f0d025243d778ea057f089150501c5eb8e205956
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-25 17:24:26 +0600

    add and pratices some quiz

--- Structure/structurequz.c
+++ Structure/structurequz.c
@@ -27,15 +27,15 @@
 //     return 0;
 // }
 
-#include<stdio.h>
-struct Course {
-    char name[];
-    int price;
-};
+// #include<stdio.h>
+// struct Course {
+//     char name[];
+//     int price;
+// };
 
-int main()
-{
-    struct Course c1 ={"C Programming",5000};
-    printf("%s",c1.name);
-    return 0;
-}
+// int main()
+// {
+//     struct Course c1 ={"C Programming",5000};
+//     printf("%s",c1.name);
+//     return 0;
+// }
