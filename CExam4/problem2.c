89f5a5be34d472967136fb1bcc45ff12bffb2822
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-26 17:06:23 +0600

    fibonacci number is shown without array is solved

--- CExam4/problem2.c
+++ CExam4/problem2.c
@@ -1,3 +1,32 @@
 /*
+ some steps of nth term of febonacci series :
 
 */
+
+
+#include<stdio.h>
+int main(){
+
+    int n,i,foboNumber;
+    scanf("%d",&n);
+   
+
+     int fib = 0;
+     int fib1 = 1; 
+ 
+    for(i=1;i<=n;i++){
+    if(i==1) printf("0 ");
+    else if(i==2) printf("1 ");  
+    else{
+   
+     foboNumber = fib+fib1; 
+      fib=fib1;
+      fib1 = foboNumber;
+     printf("%d ",foboNumber);
+    }
+
+      
+    }
+
+
+}
--- CExam4/tempCodeRunnerFile.c
+++ CExam4/tempCodeRunnerFile.c
@@ -1,12 +1,32 @@
 /*
- Ternary operator : 
+ some steps of nth term of febonacci series :
+
 */
+
 
 #include<stdio.h>
 int main(){
 
-    int a=15,b=10;
+    int n,i,foboNumber;
+    scanf("%d",&n);
+   
 
-    a>b? printf("Max=%d\n",a):printf("Max=%d\n",b);
+     int fib = 0;
+     int fib1 = 1; 
+ 
+    for(i=1;i<=n;i++){
+    if(i==1) printf("0 ");
+    else if(i==2) printf("1 ");  
+    else{
+   
+     foboNumber = fib+fib1; 
+      fib=fib1;
+      fib1 = foboNumber;
+     printf("%d ",foboNumber);
+    }
+
+      
+    }
+
 
 }
