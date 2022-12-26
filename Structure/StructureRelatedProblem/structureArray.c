caaf9c813b387f43bdce3ef88468278e9f7f57ea
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-26 10:51:29 +0600

    structure array problem is solved using function

--- Structure/StructureRelatedProblem/structureArray.c
+++ Structure/StructureRelatedProblem/structureArray.c
@@ -1,36 +1,44 @@
-#include<stdio.h>
-struct Student {
-    double marks;
-    int roll;
+#include <stdio.h>
+struct Student
+{
     char name[50];
+    int roll;
+    double marks;
 };
 
-int main(){
-   
-   struct Student s[5];
+void StudentDetails(int i, struct Student s)
+{
 
-   // get the input 
+    printf("The Information of The Students is : %d\n", i + 1);
+    printf("Name => %s\n", s.name);
+    printf("Roll => %d\n", s.roll);
+    printf("Name => %lf\n", s.marks);
+}
 
-   for (int i = 0; i < 5; i++)
-   {
-      scanf("%d\n",&s[i].roll);
-      scanf("%s\n",s[i].name);
-      scanf("%lf\n",&s[i].marks);
-      
-   }
+int main()
+{
 
+    struct Student s[5];
 
-   // print the Students info 
+    // get the input
 
     for (int i = 0; i < 5; i++)
     {
-        printf("The Information of The Students is : %d\n",i+1);
-        printf("Name => %s\n",s[i].name);
-        printf("Roll => %d\n",s[i].roll);
-        printf("Name => %lf\n",s[i].marks);
+        
+        scanf("%s\n", s[i].name);
+        scanf("%d\n", &s[i].roll);
+        scanf("%lf\n", &s[i].marks);
     }
-    
-   
+
+    // print the Students info
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        // print student info using function
+
+        StudentDetails(i, s[i]);
+    }
 
     return 0;
 }
--- Structure/StructureRelatedProblem/tempCodeRunnerFile.c
+++ Structure/StructureRelatedProblem/tempCodeRunnerFile.c
@@ -5,6 +5,14 @@ struct Student {
     char name[50];
 };
 
+void StudentDetails(int i,struct Student s){
+
+        printf("The Information of The Students is : %d\n",i+1);
+        printf("Name => %s\n",s.name);
+        printf("Roll => %d\n",s.roll);
+        printf("Name => %lf\n",s.marks);
+}
+
 int main(){
    
    struct Student s[5];
@@ -13,9 +21,10 @@ int main(){
 
    for (int i = 0; i < 5; i++)
    {
-      scanf("%lf\n",&s[i].marks);
       scanf("%d\n",&s[i].roll);
       scanf("%s\n",s[i].name);
+      scanf("%lf\n",&s[i].marks);
+      
    }
 
 
@@ -23,10 +32,12 @@ int main(){
 
     for (int i = 0; i < 5; i++)
     {
-        printf("The Information of The Students is : %d\n",i+1);
-        printf("Name => %s\n",s[i].name);
-        printf("Roll => %d\n",s[i].roll);
-        printf("Name => %lf\n",s[i].marks);
+       
+       // print student info using function
+
+        StudentDetails(i,s[i]); 
+
+
     }
     
    
