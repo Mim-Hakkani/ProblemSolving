c09c2c0b462edf3b8d1f8f3c725745bb3eaa08eb
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-27 13:15:09 +0600

    problem 4 is solved

--- CExam4/problem4.c
+++ CExam4/problem4.c
@@ -1,26 +1,19 @@
 /*
-
+  1. declare the characte of geting input 
+  2. get input usinf f gets and call the fuction initialString
+  3. calculte the length of the whole string 
+  4. sort the character using bubble sort in accending Order 
+  5. show the output of the string in assending order 
+  6. finished
 */
 
 #include<stdio.h>
 #include<string.h>
 
 void InitialString(char name[]){
-//   printf("Output is : ");
-// puts(name);
 
 int stringLength = strlen(name);
-
-// printf("Before Sort : ");
-
-//  for (int i = 0; i <stringLength; i++)
-//  {
-//     printf("%c",name[i]);
-//  }
-
-
 char temp;
-
 for(int i=0;i<stringLength;i++){
     for(int j=0;j<stringLength-1;j++){
         if(name[j]>name[j+1]){
