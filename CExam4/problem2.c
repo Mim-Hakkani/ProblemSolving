ccbb49c4cc674f1e7d9867d2e839124adb77faaa
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-12-27 12:07:13 +0600

    cExam problem 2 is solved

--- CExam4/problem2.c
+++ CExam4/problem2.c
@@ -1,32 +1,39 @@
 /*
  some steps of nth term of febonacci series :
 
+ step 1  : first declare the variable of first two fibo number 
+ step 2  : loop executed in n times
+ step 3 : add contion of first two fibo number 
+ step 4 : add two declate variable 
+ step 5 : swap the fibo and fibo1 
+ step 6 : execute n times 
+ step 7 : end 
+
 */
 
+#include <stdio.h>
+int main()
+{
 
-#include<stdio.h>
-int main(){
+    int n, i, foboNumber;
+    scanf("%d", &n);
 
-    int n,i,foboNumber;
-    scanf("%d",&n);
-   
+    int fib = 0;
+    int fib1 = 1;
 
-     int fib = 0;
-     int fib1 = 1; 
- 
-    for(i=1;i<=n;i++){
-    if(i==1) printf("0 ");
-    else if(i==2) printf("1 ");  
-    else{
-   
-     foboNumber = fib+fib1; 
-      fib=fib1;
-      fib1 = foboNumber;
-     printf("%d ",foboNumber);
+    for (i = 1; i <= n; i++)
+    {
+        if (i == 1)
+            printf("0 ");
+        else if (i == 2)
+            printf("1 ");
+        else
+        {
+
+            foboNumber = fib + fib1;
+            fib = fib1;
+            fib1 = foboNumber;
+            printf("%d ", foboNumber);
+        }
     }
-
-      
-    }
-
-
 }
