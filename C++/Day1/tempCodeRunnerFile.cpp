8f3be58cb57a203e1f85a4e2838325bff45247cb
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-01 18:50:28 +0600

    get input output fron string

--- C++/Day1/tempCodeRunnerFile.cpp
+++ C++/Day1/tempCodeRunnerFile.cpp
@@ -4,9 +4,12 @@ int main(){
     int num;
     char letter;
     float price;
-    string name;
+    string name,lastname,fullname;
     cin>>name>>letter>>price>>num;
     cout<<"Name : "<<name<<"\n"<<"Letter is : "<<letter<<"\n"<<"Price : "<<price<<"\nNumber : "<<num<<"\n";
+    cin>>lastname;
+    fullname = name + lastname ;
+    cout<<"Full name : "<<fullname<<"\n";
     
     return 0;
 }
