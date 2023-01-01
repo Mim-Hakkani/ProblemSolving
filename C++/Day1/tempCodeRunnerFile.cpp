29b48be227e80e518422cbca0873009f670a5e09
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-01 18:15:22 +0600

    basic input output in c++

---
+++ C++/Day1/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    char letter;
+    float price;
+    string name;
+    cin>>name>>letter>>price>>num;
+    cout<<"Name : "<<name<<"\n"<<"Letter is : "<<letter<<"\n"<<"Price : "<<price<<"\nNumber : "<<num<<"\n";
+    
+    return 0;
+}
