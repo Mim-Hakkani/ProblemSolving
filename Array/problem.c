53effa78b55c1d6b59acb4977bb8b8d74de37d46
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-07-15 21:35:04 +0600

    Array problem set using c programming

---
+++ Array/problem.c
@@ -0,0 +1,125 @@
+Sure! Here are 150 array problems in increasing order of difficulty:
+
+Easy Level:
+1. Write a program to find the sum of all elements in an array.
+2. Write a program to find the maximum element in an array.
+3. Write a program to find the minimum element in an array.
+4. Write a program to calculate the average of elements in an array.
+5. Write a program to reverse the elements of an array.
+6. Write a program to find the sum of even numbers in an array.
+7. Write a program to find the sum of odd numbers in an array.
+8. Write a program to find the number of even numbers in an array.
+9. Write a program to find the number of odd numbers in an array.
+10. Write a program to check if an array is sorted in ascending order.
+
+Intermediate Level:
+11. Write a program to find the second largest element in an array.
+12. Write a program to find the second smallest element in an array.
+13. Write a program to find the sum of elements at even indices in an array.
+14. Write a program to find the sum of elements at odd indices in an array.
+15. Write a program to find the product of all elements in an array.
+16. Write a program to check if an array is a palindrome.
+17. Write a program to find the frequency of a given element in an array.
+18. Write a program to remove duplicate elements from an array.
+19. Write a program to find the sum of all positive elements in an array.
+20. Write a program to find the sum of all negative elements in an array.
+
+Advanced Level:
+21. Write a program to find the kth largest element in an array.
+22. Write a program to find the kth smallest element in an array.
+23. Write a program to sort an array in ascending order using the bubble sort algorithm.
+24. Write a program to sort an array in descending order using the bubble sort algorithm.
+25. Write a program to find the median of an array.
+26. Write a program to find the mode of an array.
+27. Write a program to find the largest subarray with contiguous elements.
+28. Write a program to find the smallest missing positive number in an array.
+29. Write a program to rotate an array by k positions.
+30. Write a program to find the maximum sum subarray in an array.
+
+Expert Level:
+31. Write a program to find the maximum product subarray in an array.
+32. Write a program to find the longest increasing subarray in an array.
+33. Write a program to find the longest common subarray between two arrays.
+34. Write a program to merge two sorted arrays into a single sorted array.
+35. Write a program to find the number of pairs in an array whose sum is equal to a given target.
+36. Write a program to find the smallest subarray with a sum greater than or equal to a given target.
+37. Write a program to find the largest subarray with a sum less than or equal to a given target.
+38. Write a program to find the maximum sum subrectangle in a 2D array.
+39. Write a program to find the maximum product subrectangle in a 2D array.
+40. Write a program to find the longest common subsequence between two arrays.
+
+Challenging Level:
+41. Write a program to find the majority element in an array (element occurring more than n/2 times).
+42. Write a program to find the kth smallest element in a row-wise and column-wise sorted 2D array.
+43. Write a program to find the number of subarrays with a sum divisible by k.
+44. Write a program to find the longest subarray with an equal number of 0s and 1s.
+45. Write a program to find the maximum number of non-overlapping subarrays with a sum equal to a given target.
+46. Write a program to find the maximum subarray sum with no adjacent elements.
+47. Write a program to find the minimum number of platforms required for a railway station given arrival and departure times.
+48. Write a program to find the longest increasing subsequence in an array.
+49. Write a program to find the longest bitonic subarray in an array.
+50. Write a program to find the length of the shortest unsorted subarray that, when sorted, makes the entire array sorted.
+
+Expert+ Level:
+51. Write a program to find the maximum sum subsequence with no adjacent elements.
+52. Write a program to find the maximum profit by buying and selling stocks from an array of prices.
+53. Write a program to find the minimum number of jumps required to reach the end of an array.
+54. Write a program to find the length of the longest increasing subsequence in an array.
+55. Write a program to find the length of the longest bitonic subsequence in an array.
+56. Write a program to find the length of the longest alternating subsequence in an array.
+57. Write a program to find the maximum sum subarray with at most one element deletion.
+58. Write a program to find the maximum sum subarray with at most one element modification.
+59. Write a program to find the maximum sum subarray with at most one element rotation.
+60. Write a program to find the maximum sum subarray with at most k element deletions.
+
+Champion Level:
+61. Write a program to find the maximum sum subarray with at most k element modifications.
+62. Write a program to find the maximum sum subarray with at most k element rotations.
+63. Write a program to find the maximum sum subarray with at most k element replacements.
+64. Write a program to find the length of the longest increasing subsequence with at most one element deletion.
+65. Write a program to find the length of the longest increasing subsequence with at most one element modification.
+66. Write a program to find the length of the longest increasing subsequence with at most one element rotation.
+67. Write a program to find the length of the longest increasing subsequence with at most k element deletions.
+68. Write a program to find the length of the longest increasing subsequence with at most k element modifications.
+69. Write a program to find the length of the longest increasing subsequence with at most k element rotations.
+70. Write a program to find the length of the longest increasing subsequence with at most k element replacements.
+
+Grandmaster Level:
+71. Write a program to find the length of the longest bitonic subsequence with at most one element deletion.
+72. Write a program to find the length of the longest bitonic subsequence with at most one element modification.
+73. Write a program to find the length of the longest bitonic subsequence with at most one element rotation.
+74. Write a program to find the length of the longest bitonic subsequence with at most k element deletions.
+75. Write a program to find the length of the longest bitonic subsequence with at most k element modifications.
+76. Write a program to find the length of the longest bitonic subsequence with at most k element rotations.
+77. Write a program to find the length of the longest bitonic subsequence with at most k element replacements.
+78. Write a program to find the length of the longest alternating subsequence with at most one element deletion.
+79. Write a program to find the length of the longest alternating subsequence with at most one element modification.
+80. Write a program to find the length of the longest
+
+ alternating subsequence with at most one element rotation.
+
+Elite Level:
+81. Write a program to find the length of the longest alternating subsequence with at most k element deletions.
+82. Write a program to find the length of the longest alternating subsequence with at most k element modifications.
+83. Write a program to find the length of the longest alternating subsequence with at most k element rotations.
+84. Write a program to find the length of the longest alternating subsequence with at most k element replacements.
+85. Write a program to find the maximum sum subarray with at most one element deletion and one element modification.
+86. Write a program to find the maximum sum subarray with at most one element deletion and one element rotation.
+87. Write a program to find the maximum sum subarray with at most one element deletion and one element replacement.
+88. Write a program to find the maximum sum subarray with at most one element modification and one element rotation.
+89. Write a program to find the maximum sum subarray with at most one element modification and one element replacement.
+90. Write a program to find the maximum sum subarray with at most one element rotation and one element replacement.
+
+Legend Level:
+91. Write a program to find the maximum sum subarray with at most k element deletions and one element modification.
+92. Write a program to find the maximum sum subarray with at most k element deletions and one element rotation.
+93. Write a program to find the maximum sum subarray with at most k element deletions and one element replacement.
+94. Write a program to find the maximum sum subarray with at most k element modifications and one element rotation.
+95. Write a program to find the maximum sum subarray with at most k element modifications and one element replacement.
+96. Write a program to find the maximum sum subarray with at most k element rotations and one element replacement.
+97. Write a program to find the length of the longest increasing subsequence with at most one element deletion, one element modification, and one element rotation.
+98. Write a program to find the length of the longest increasing subsequence with at most k element deletions, one element modification, and one element rotation.
+99. Write a program to find the length of the longest bitonic subsequence with at most one element deletion, one element modification, and one element rotation.
+100. Write a program to find the length of the longest bitonic subsequence with at most k element deletions, one element modification, and one element rotation.
+
+These array problems cover a wide range of challenges and can help you improve your programming skills using C syntax. Good luck with your programming journey!
