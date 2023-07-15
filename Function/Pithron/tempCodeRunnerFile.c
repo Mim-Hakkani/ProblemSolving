666069d8637f65dbdf2a2660c6e888a7479ead9d
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-07-15 21:23:05 +0600

    function problem set

--- Function/Pithron/tempCodeRunnerFile.c
+++ Function/Pithron/tempCodeRunnerFile.c
@@ -1,48 +1,46 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+// int fact(int a){
+//     int i,fact = 1;
+//     for(i=1;i<=a;i++){
+//         fact*=i;
+      
+//     }
+//       return fact;
+// }
+// int main(){
+//     int a,r;
+//     scanf("%d %d",&a,&r);
+//     fact(a);
+
+//     int permutaion = fact(a)/fact(a-r);
+//     int combinator = fact(a)/fact(a-r)*fact(r);
+
+
+
+//     printf("Permutation : %d\n",permutaion);
+//     printf("Combinator : %d\n",combinator);
+
+
+//     return 0;
+// }
+
 #include<stdio.h>
+int addition();
 
-// input for test case 
-
-void inputTest(int test,int a[]){
-
- for(int i =0;i<test;i++){
-        scanf("%d",&a[i]);
-    }
-
+int main()
+{
+    int result;
+    result = addition();
+    printf("%d",result*2);
+    return 0;
 }
 
 
-// void grater more or less value 
-
-int greaterMoreLess(int test , int searchValue, int a[]){
-    int count=0 ;
-
-  for(int j=0;j<test;j++){
-    if(a[j]!=searchValue){
-        count++;
-    }
-  }
-
-  return count;
-}
-
-int main(){
-    int test , a[100], searchValue;
-    scanf("%d",&test);
-
-   // function for input number 
-    inputTest(test,a);
-
-  scanf("%d",&searchValue);
-
-
-//   greaterMore or Less 
-
-  int counter = greaterMoreLess(test,searchValue,a);
-  
-
-  printf("Less Or Grater Value is : %d\n",counter);
-  return 0;
-
-
-
+int addition()
+{
+    int num1 = 10, num2 = 5;
+    return num1+num2;
 }
