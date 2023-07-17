20cc57336931c6044e3b9d76a535f8bc8281fd44
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-07-17 06:54:48 +0600

    check divisible by 2 or 3

---
+++ Condition/problemSolution/8.divisible2and3.c
@@ -0,0 +1,29 @@
+// problem Name : Write a program to find the maximum among three numbers.
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter three number : ");
+    scanf("%d", &number);
+    if (number % 2 == 0 && number % 3 == 0)
+    {
+        printf("%d Divisible by 2 and 3  \n", number);
+    }
+
+    else
+    {
+        printf("%d Not Divisible by 2 and 3\n", number);
+    }
+
+    return 0;
+}
+
+/*
+input :
+Enter 5
+Enter 15
+
+output :
+nor Divisible
+Divisible
+*/
--- Condition/problemSolution/tempCodeRunnerFile.c
+++ Condition/problemSolution/tempCodeRunnerFile.c
@@ -5,14 +5,14 @@ int main()
     int number;
     printf("Enter three number : ");
     scanf("%d", &number);
-    if (number % 2 == 0 || number % 3 == 0)
+    if (number % 2 == 0 && number % 3 == 0)
     {
-        printf("%d Divisible\n", number);
+        printf("%d Divisible by 2 and 3  \n", number);
     }
 
     else
     {
-        printf("%d Not Divisible\n", number);
+        printf("%d Not Divisible by 2 and 3\n", number);
     }
 
     return 0;
